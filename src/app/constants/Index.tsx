2ab66d131b496073e97ab433aec1569067755ba7
unknown <howell2028@yahoo.com>
2024-10-17 16:03:05 +0100

    modification

--- src/app/constants/Index.tsx
+++ src/app/constants/Index.tsx
@@ -1,5 +1,6 @@
 export const ourWork = [
     {path: "/static/d6e6c11b-ad59-4f0f-86f4-9b83ea8fca6a.jfif", title: "Fruit", content: "Breakout on the upside. Entry Point after the first bullish mommentum candlestick"},
     {path: "/static/39607d82-ef0c-4059-a42d-e2f8c284ec23.jfif", title: "Fruit", content: "Breakout on the upside. Entry Point after the first bullish mommentum candlestick"},
-    {path: "/static/1b35c8d4-4d3b-4336-a2f7-80d164a087f4.jfif", title: "Fruit", content: "Breakout on the upside. Entry Point after the first bullish mommentum candlestick"}
+    {path: "/static/1b35c8d4-4d3b-4336-a2f7-80d164a087f4.jfif", title: "Fruit", content: "Breakout on the upside. Entry Point after the first bullish mommentum candlestick"},
+    {path: "/static/IMG_0358.PNG", title: "Fruit", content: "Breakout on the upside. Entry Point after the first bullish mommentum candlestick"}
 ]
