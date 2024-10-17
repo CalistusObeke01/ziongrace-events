a44c14a65de261076efe3b5235d55988f6c61252
unknown <howell2028@yahoo.com>
2024-10-17 13:25:40 +0100

    modification

--- src/app/constants/Index.tsx
+++ src/app/constants/Index.tsx
@@ -1,4 +1,4 @@
-export let ourWork = [
+export const ourWork = [
     {path: "https://www.youtube.com/shorts/oHKmm0jkY_Y", chart_pattern: "Wedge", content: "Breakout on the upside. Entry Point after the first bullish mommentum candlestick"},
     {path: "https://www.youtube.com/shorts/zkXBHBSZQio", chart_pattern: "Trendline", content: "Trendline broken and retested before moving to the upside"},
     {path: "https://www.youtube.com/shorts/zkXBHBSZQio", chart_pattern: "Double Top", content: "The price action broke the neckline and retested before pushing downside"},
